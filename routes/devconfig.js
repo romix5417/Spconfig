8025828e304db6d65573fd88b0944bbd5eb8cbda
root <root@ETWS.lan>
2019-04-19 19:17:06 +0800

    1.add the communication protocol about nodejs program with the RadarGsp.

--- routes/devconfig.js
+++ routes/devconfig.js
@@ -1,40 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const net = require('net');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
+    req.data = {
+        "rangbin": "0",
+        "prt": "0",
+        "azimuth":"0"
+    };
+
+    const client = net.createConnection({path:'/tmp/server.sock'}, () => {
+        //'connect' listener
+        console.log('connected to server');
+        client.write("get .hostname");
+    });
+
+    client.on('data', (data) => {
+        console.log(data.toString());
+        client.end();
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server.');
+    });
+
     res.render('devconfig', req);
 });
 
 router.get('/interboard', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
     res.render('interboardconf', req);
 });
 
 router.get('/adboard', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
     res.render('adboardconf', req);
 });
 
 router.get('/upgrade', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
     res.render('upgrade', req);
 });
 
 router.get('/power', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
     res.render('power', req);
 });
 
 router.post('/upgrade/upload', function(req, res, next) {
     req.title = "雷达信处控制系统";
-    req.curTime = new Date();
     res.render('upgrade', req);
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,59 @@
 var express = require('express');
 var router = express.Router();
+const net = require('net');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     req.title = "雷达信处控制系统";
     req.curTime = new Date();
-    res.render('index', req);
+    req.data = {
+        "name": "",
+        "firmware": "",
+        "hostname": "",
+        "mainboard_cpu_arch":"",
+        "mainboard_cpu_info":"",
+        "mainboard_system_os":"",
+        "mainboard_system_kernel":"",
+        "mainboard_system_uptime":"",
+        "mainboard_memory_total":"",
+        "mainboard_memory_used":"",
+        "mainboard_memory_free":""
+    };
+
+    const client = net.createConnection({path:'/tmp/server.sock'}, () => {
+        //'connect' listener
+        console.log('connected to server');
+        client.write("get .");
+    });
+
+    client.on('data', (data) => {
+        console.log(data.toString());
+        data = JSON.parse(data);
+
+        req.data.name = data.name;
+        req.data.firmware = data.firmware;
+        req.data.hostname = data.hostname;
+        req.data.mainboard_cpu_arch = data.mainboard.cpu.arch;
+        req.data.mainboard_cpu_info = data.mainboard.cpu.info;
+        req.data.mainboard_system_os = data.mainboard.system.os;
+        req.data.mainboard_system_kernel = data.mainboard.system.kernel_ver;
+        req.data.mainboard_system_uptime = data.mainboard.system.uptime;
+        req.data.mainboard_memory_total = data.mainboard.memory.total;
+        req.data.mainboard_memory_used = data.mainboard.memory.used;
+        req.data.mainboard_memory_free = data.mainboard.memory.free;
+
+        console.log(req.data.mainboard_cpu_arch);
+
+        res.render('index', req);
+        client.end();
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server.');
+    });
+
+
 });
 
 module.exports = router;
