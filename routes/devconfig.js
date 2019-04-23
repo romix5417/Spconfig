968526eac5a82e8bc05aeb6c27e77e28d336bc4e
root <root@ETWS.lan>
2019-04-23 19:07:48 +0800

    1.add communicate protocol base of json config file.

--- routes/devconfig.js
+++ routes/devconfig.js
@@ -2,32 +2,91 @@ var express = require('express');
 var router = express.Router();
 const net = require('net');
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+function get_radargsp_info(req, res, next) {
     req.title = "雷达信处控制系统";
     req.data = {
-        "rangbin": "0",
-        "prt": "0",
-        "azimuth":"0"
+        "radargsp_radar_rffreq": "0",
+        "radargsp_radar_rangebin": "0",
+        "radargsp_radar_prtnum":"0",
+        "radargsp_radar_rchanel":"0",
+        "radargsp_radar_azimuth":"0",
+        "radargsp_count_ksnrzdb":"0",
+        "radargsp_count_ksnrvdb":"0",
+        "radargsp_count_windows":"0",
+        "radargsp_count_radarconst":"0",
+        "radargsp_running_cache":"0",
+        "radargsp_running_threadpool":"0",
+        "radargsp_running_clutter_filter":"0"
     };
 
     const client = net.createConnection({path:'/tmp/server.sock'}, () => {
         //'connect' listener
         console.log('connected to server');
-        client.write("get .hostname");
+        client.write("get .radargsp");
     });
 
     client.on('data', (data) => {
-        console.log(data.toString());
+        data = JSON.parse(data);
+
+        req.data.radargsp_radar_rffreq = data.radar.rffreq;
+        req.data.radargsp_radar_rangebin = data.radar.rangebin;
+        req.data.radargsp_radar_prtnum = data.radar.prtnum;
+        req.data.radargsp_radar_rchanel = data.radar.rchanel;
+        req.data.radargsp_radar_azimuth = data.radar.azimuth;
+        req.data.radargsp_count_ksnrzdb = data.count.ksnrzdb;
+        req.data.radargsp_count_ksnrvdb = data.count.ksnrvdb;
+        req.data.radargsp_count_windows = data.count.windows;
+        req.data.radargsp_count_radarconst = data.count.radarconst;
+        req.data.radargsp_running_cache = data.running.cache;
+        req.data.radargsp_running_threadpool = data.running.threadpool;
+        req.data.radargsp_running_clutter_filter = data.running.clutter_filter;
+
+        res.render('devconfig', req);
+        client.end();
+    });
+
+    client.setTimeout(100, function () {
+        console.log('客户端在' + 100 + 'ms内未通信，将断开连接...');
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server.');
+    });
+}
+
+function post_radargsp_info(req, res, path, postdata) {
+    const client = net.createConnection({path:'/tmp/server.sock'}, () => {
+        //'connect' listener
+        console.log('connected to server');
+        var cmd = "set \'";
+
+        for (var i = 0; i < path.length; i++) {
+            cmd += path[i] + "=" + "\"" + postdata[i] + "\"";
+            if (i < (path.length - 1)) {
+                cmd += " | "
+            }
+        }
+
+        cmd += "\'";
+        client.write(cmd);
+    });
+
+    client.setTimeout(100, function () {
+        console.log('客户端在' + 100 + 'ms内未通信，将断开连接...');
+    });
+
+    client.on('data', (data) => {
         client.end();
     });
 
     client.on('end', () => {
         console.log('disconnected from server.');
     });
+}
 
-    res.render('devconfig', req);
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    get_radargsp_info(req,res, next);
 });
 
 router.get('/interboard', function(req, res, next) {
@@ -53,6 +112,24 @@ router.get('/power', function(req, res, next) {
 router.post('/upgrade/upload', function(req, res, next) {
     req.title = "雷达信处控制系统";
     res.render('upgrade', req);
+});
+
+router.post('/runningpost', function (req, res, next) {
+
+    var path = [
+        ".radargsp.running.cache",
+        ".radargsp.running.threadpool",
+        ".radargsp.running.clutter_filter"
+    ];
+
+    var postdata = [];
+    console.log(req.body.cache);
+    postdata[0] = req.body.cache;
+    postdata[1] = req.body.threadpool;
+    postdata[2] = req.body.clutter_filter;
+
+    post_radargsp_info(req, res, path, postdata);
+    get_radargsp_info(req, res, next);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const net = require('net');
+const os = require('os');
 
 
 /* GET home page. */
@@ -13,13 +14,15 @@ router.get('/', function(req, res, next) {
         "hostname": "",
         "mainboard_cpu_arch":"",
         "mainboard_cpu_info":"",
+        "mainboard_cpu_threadx":"",
         "mainboard_system_os":"",
         "mainboard_system_kernel":"",
         "mainboard_system_uptime":"",
         "mainboard_memory_total":"",
         "mainboard_memory_used":"",
         "mainboard_memory_free":"",
-        "mainboard_memory_cache":""
+        "mainboard_memory_cache":"",
+        "average_load":""
     };
 
     const client = net.createConnection({path:'/tmp/server.sock'}, () => {
@@ -29,7 +32,7 @@ router.get('/', function(req, res, next) {
     });
 
     client.on('data', (data) => {
-        console.log(data.toString());
+        //console.log(data.toString());
         data = JSON.parse(data);
 
         req.data.name = data.name;
@@ -37,6 +40,7 @@ router.get('/', function(req, res, next) {
         req.data.hostname = data.hostname;
         req.data.mainboard_cpu_arch = data.mainboard.cpu.arch;
         req.data.mainboard_cpu_info = data.mainboard.cpu.info;
+        req.data.mainboard_cpu_threadx = data.mainboard.cpu.threadx;
         req.data.mainboard_system_os = data.mainboard.system.os;
         req.data.mainboard_system_kernel = data.mainboard.system.kernel_ver;
         req.data.mainboard_system_uptime = data.mainboard.system.uptime;
@@ -45,7 +49,11 @@ router.get('/', function(req, res, next) {
         req.data.mainboard_memory_free = data.mainboard.memory.free;
         req.data.mainboard_memory_cache = data.mainboard.memory.cache;
 
-        console.log(req.data.mainboard_cpu_arch);
+        var load_avg = os.loadavg();
+        load_avg[0] = load_avg[0].toFixed(2);
+        load_avg[1] = load_avg[1].toFixed(2);
+        load_avg[2] = load_avg[2].toFixed(2);
+        req.data.average_load = "" + load_avg;
 
         res.render('index', req);
         client.end();
